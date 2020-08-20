7d54f22496ff55524d1a45c3c05e9339988e0f74
Asger Schultz <asger.s@protonmail.com>
2020-08-21 00:44:51 +0200

    Fix rust integration

--- src/_ctype_rust.py
+++ src/_ctype_rust.py
@@ -1,5 +1,11 @@
 import os, sys
 import ctypes
 
-rust_lib = ctypes.CDLL( os.path.dirname( sys.argv[0] ), 'src' 'rust_src', 'liblib.so' )
-rootloop = rust_lib._FuncPtr
+rust_lib = ctypes.CDLL(
+    os.path.join(
+        os.path.dirname(sys.argv[0]),
+        'src', 'rust_src', 'target', 'release', 'librust_src.so'
+    )
+)
+rootloop = rust_lib.rootloop
+
