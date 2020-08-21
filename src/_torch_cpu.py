e9c7f0b60a1bb5b41896e1c64536c28049a74467
sorenmulli <swholm@protonmail.com>
2020-08-21 15:52:43 +0200

    Fix pyTorch dtype

--- src/_torch_cpu.py
+++ src/_torch_cpu.py
@@ -1,4 +1,4 @@
 import torch
 def rootloop(N):
-	# Coerce to float as pyTorch has not defined square root for ints
-	return torch.sum( torch.sqrt( torch.arange(0.0, N, 2, device=torch.device('cpu')) ) )
+	# Coerce to double as pyTorch defaults to float32 otherwise
+	return torch.sum( torch.sqrt( torch.arange(0, N, 2, device=torch.device('cpu'), dtype=torch.double) ) )
